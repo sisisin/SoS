92481c1f280c1c79789f5e8c64939b1eebf39bb6
sisisin <azsisisin@gmail.com>
2016-02-13 10:46:47 +0900

    add readme, fix indent

---
+++ README.md
@@ -0,0 +1,5 @@
+# SoS
+# TODO
+* u,p,m save
+* memo save
+* alphabet sort
--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,25 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+'use strict';
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+const app = {
+  initialize: function () {
+    this.bindEvents();
+  },
+  bindEvents: function () {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  onDeviceReady: function () {
+    app.receivedEvent('deviceready');
+  },
+  receivedEvent: function (id) {
+    const parentElement = document.getElementById(id);
+    const listeningElement = parentElement.querySelector('.listening');
+    const receivedElement = parentElement.querySelector('.received');
 
-        console.log('Received Event: ' + id);
-    }
-};
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  }
+}
 
 app.initialize();
